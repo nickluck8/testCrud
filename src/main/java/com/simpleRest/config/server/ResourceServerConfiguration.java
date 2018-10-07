18e661cc55ad2f19ed54bcfae94e802d91a922dd
Nikolay Steliuk <nikolaysteliuk@gmail.com>
2018-10-07 16:56:04 +0300

    readme added

--- src/main/java/com/simpleRest/config/server/ResourceServerConfiguration.java
+++ src/main/java/com/simpleRest/config/server/ResourceServerConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
-    private static final String RESOURCE_ID = "resource-server-rest-api";
+    private static final String RESOURCE_ID = "resource-server";
     private static final String SECURED_READ_SCOPE = "#oauth2.hasScope('read')";
     private static final String SECURED_WRITE_SCOPE = "#oauth2.hasScope('write')";
     private static final String SECURED_PATTERN = "/secured/**";
